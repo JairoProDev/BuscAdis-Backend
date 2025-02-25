4bcfd09741145eb56551f62db452366ceae0e480
Jairo Saul <JairoProDev@gmail.com>
2025-02-25 12:48:52 -0500

    Add RequestWithUser interface for flexible request typing
    - Create new RequestWithUser interface extending Request
    - Make user property optional to support scenarios with undefined user
    - Complement existing AuthenticatedRequest type for more flexible request handling
    error 81

--- src/common/types/request.type.ts
+++ src/common/types/request.type.ts
@@ -3,4 +3,8 @@ import { User } from '../../modules/users/entities/user.entity';
 
 export interface AuthenticatedRequest extends Request {
   user: User;
+}
+
+export interface RequestWithUser extends Request {
+  user?: User;
 } 
